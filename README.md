9e1ddf5c4113ffd3babc654bbbf933c7dc33e638
jsee-twilio <ychian@gmail.com>
2020-11-09 21:30:42 +0800

    First

---
+++ README.md
@@ -0,0 +1,1 @@
+# abrsm2email
---
+++ api/email2email.js
@@ -0,0 +1,39 @@
+// Email to Email
+const util = require('util');
+const multer = require('multer');
+const addrs = require("email-addresses");
+const sgMail = require('@sendgrid/mail');
+
+module.exports = async (req, res) => { 
+    await util.promisify(multer().any())(req, res);
+
+    const from = req.body.from;
+    const subject = req.body.subject;
+    const body = req.body.text;
+    const html = req.body.html;
+
+    // Strip for email 
+    const fromAddress = addrs.parseOneAddress(from);
+
+    // SendGrid API
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    
+    // Create Email
+    const email = {
+        to: process.env.TO_EMAIL_ADDRESS,
+        from: process.env.FROM_EMAIL_ADDRESS,
+        subject: process.env.FROM_EMAIL_ADDRESS + `${subject}`,
+        text: `${body}`,
+        html: `${html}`,
+    };
+
+    //Send Email
+    sgResp = sgMail.send(email)
+        .then(response => {
+            res.status(200).send(`Sent Email`);
+        })
+        .catch(error => {
+            res.status(500);
+        });
+    // res.status(200).send(`Sent Email`);
+};
